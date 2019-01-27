9de733318540c9529f91c379052ecee5e253bd26
Jason <jason127dn@gmail.com>
2019-01-27 16:00:48 +0800

    build cnn net

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 from zipfile import ZipFile
 from PIL import Image
 
-batch_size = 500
+batch_size = 100
 
 
 test_path = 'data/test.zip'
@@ -25,15 +25,81 @@ test_set['id'] = test_set.infol.map(lambda x: x.filename.split('.')[0])
 
 def load_data(zip_path, _df):
 
-    X = np.zeros([len(_df), 96, 96, 3])
+    X = np.zeros([0, 96, 96, 3])
     with ZipFile(zip_path, 'r') as zp:
-        for i in range(len(_df)):
+        for i in _df.index:
             with zp.open(_df.infol[i]) as imagefile:
                 img = Image.open(imagefile)
-                #img.show()
-                X[i] = np.array(img)
+                # img.show()
+                X = np.concatenate((X, [np.array(img)]), axis=0)
 
     return X
 
-x = load_data(test_path, test_set[0:10])
 
+input_x = tf.placeholder(dtype=tf.float32, shape=[None, 96, 96, 3])
+input_x = input_x/128-1
+input_y = tf.placeholder(dtype=tf.float32, shape=[None, 1])
+
+conv1 = tf.layers.conv2d(
+    inputs=input_x,
+    filters=50,
+    kernel_size=(3, 3),
+    padding='same',
+    activation=tf.nn.relu)
+p1 = tf.layers.max_pooling2d(
+    inputs=conv1,
+    pool_size=(2, 2),
+    strides=(2, 2))
+
+conv2 = tf.layers.conv2d(
+    inputs=p1,
+    filters=100,
+    kernel_size=(3, 3),
+    padding='same',
+    activation=tf.nn.relu)
+p2 = tf.layers.max_pooling2d(
+    inputs=conv2,
+    pool_size=(2, 2),
+    strides=(2, 2))
+
+conv3 = tf.layers.conv2d(
+    inputs=p2,
+    filters=100,
+    kernel_size=(3, 3),
+    padding='same',
+    activation=tf.nn.relu)
+p3 = tf.layers.max_pooling2d(
+    inputs=conv3,
+    pool_size=(2, 2),
+    strides=(2, 2))
+
+f1 = tf.layers.flatten(inputs=p3)
+d1 = tf.layers.dense(inputs=f1, units=100, activation=tf.nn.relu)
+
+y = tf.layers.dense(inputs=d1, units=1, activation=tf.nn.sigmoid)
+loss = tf.losses.mean_squared_error(labels=input_y, predictions=y)
+opt = tf.train.AdamOptimizer()
+train = opt.minimize(loss)
+
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+
+def training(x_, y_):
+    t, l = sess.run([train, loss], feed_dict={input_x: x_, input_y: y_})
+    print(l)
+
+
+batch_n = int(len(train_set)/batch_size)
+
+for epo in range(10):
+    print('in epo:', epo)
+    for i in range(batch_n):
+
+        xx = train_set[i*batch_size:(i+1)*batch_size]
+        x_batch = load_data(
+            train_path, xx)
+        y_batch = np.array(
+            train_set.label[i*batch_size:(i+1)*batch_size]).reshape([-1, 1])
+        training(x_batch, y_batch)
